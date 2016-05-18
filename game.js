89c80f4bae7f7f7b2967287cf9627eaf760dfe05
Rich Churcher <rich.churcher@gmail.com>
2016-05-19 10:57:17 +1200

    Scoring spare frames

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) {
+function scoreFrame (frame, nextFrame) {
+  var score = frame[0] + frame[1]
+  if (score === 10) {
+    return 10 + nextFrame[0]
+  }
   return frame[0] + frame[1]
 }
--- tests/index.js
+++ tests/index.js
@@ -19,3 +19,11 @@ test('scores an open frame', function (t) {
   t.equals(score, 8, 'scores an open frame')
   t.end()
 })
+
+test('scores a spare frame', function (t) {
+  var frame = [5, 5]
+  var nextFrame = [3, 5]
+  var score = game.scoreFrame(frame, nextFrame)
+  t.equals(score, 13, 'scores a spare frame')
+  t.end()
+})
